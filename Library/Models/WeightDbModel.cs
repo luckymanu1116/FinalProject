937298d280ecb1c3ded06cc6dd43cbdf7a30a876
Manoj Reddy <luckymanoj1116@gmail.com>
2023-04-11 22:18:30 -0400

    added more db models

---
+++ Library/Models/WeightDbModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+	public class WeightDbModel
+	{
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string _id { get; set; }
+
+        [Required]
+        public string Weight { get; set; }
+
+        [Required]
+        public String Date { get; set; }
+
+        [Required]
+        public String UserId { get; set; }
+    }
+}
+
---
+++ Library/Repository/WeightRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Library.Repository
+{
+	public class WeightRepository
+	{
+		public WeightRepository()
+		{
+		}
+	}
+}
+
